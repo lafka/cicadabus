d40a9d8456dff80f81706a4425833b877abdcf36
Olav Frengstad <olav@fwt.no>
2022-04-29 14:00:09 +0200

    cicadabus-v0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CicadaBus.MixProject do
   def project do
     [
       app: :cicadabus,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
